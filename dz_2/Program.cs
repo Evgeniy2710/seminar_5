5714122c2da73b81248390ed76422d0a04cc8454
Evgeniy <evalrez@mail.ru>
2024-02-01 15:36:30 +0300

    add dz_2

--- dz_2/Program.cs
+++ dz_2/Program.cs
@@ -7,47 +7,51 @@ class UserInputToCompileForTest
     public static void PrintArray(int[,] array)
     {
         //Напишите свое решение здесь
-         for (int i = 0; i < array.Length; i++)
+
+        // Первый фор для индекса строк
+        for (int i = 0; i < array.GetLength(0); i++)
         {
-            for (int j = 0; j < array.Length; j++)
-            {
-                Console.Write($"{array[i,j]}\t");
-            }
+            // Второй фор для индекса столбцов
             Console.WriteLine();
-
-	}
-
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                // Вывод на экран
+                Console.Write(array[i, j] + "\t");
+            }
+        }
     }
 
-// Обмен первой с последней строкой
+    // Обмен первой с последней строкой
     public static int[,] SwapFirstLastRows(int[,] array)
     {
         //Напишите свое решение здесь
-        int f = array.GetLength(0);
-        int l = array.GetLength(0)+2;
-        int temp = 0;
-        
-            for (int i = 1; i < array.Length-1; i++)
-            {
 
-                temp = array[f];
-                array[f] = array[l,i];
-                array[l,i] = temp;
-            }
+        int FirstRow = 0;
+        // Индекс последний строки
+        int LastRow = array.GetLength(0) - 1;
+
+        // Цикл который пробегает по числу элементов в строке
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            // Мы из одной строки берём значение, кладём его в буфер, на его место кладём значение из другой строки, а туда кладём значение из буфера как бы поменяв их местами
+            int buff = array[FirstRow, i];
+            array[FirstRow, i] = array[LastRow, i];
+            array[LastRow, i] = buff;
+        }
+        // Возврат массива
         return array;
     }
 
-// Обмен элементами массива
+    // Обмен элементами массива
     public static void SwapItems(int[,] array, int i)
     {
-       //Напишите свое решение здесь
+        //Напишите свое решение здесь
     }
 
     public static void PrintResult(int[,] numbers)
     {
         //Напишите свое решение здесь
-        SwapFirstLastRows(numbers);
-        PrintArray(numbers);
+        PrintArray( SwapFirstLastRows(numbers));
     }
 }
 
@@ -94,7 +98,7 @@ class Answer
                 {1, 2, 3, 4},
                 {5, 6, 7, 8},
                 {9, 10, 11, 12}
-            }; 
+            };
         }
         UserInputToCompileForTest.PrintResult(numbers);
     }
